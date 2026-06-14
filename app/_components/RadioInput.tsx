3c1a13af1ce58c0c1a658683e37a5a797d42fd34
Danw567 <danielwiddup7@gmail.com>
2026-06-14 22:35:17 +0100

    section working and styled

--- app/_components/RadioInput.tsx
+++ app/_components/RadioInput.tsx
@@ -15,12 +15,12 @@ export default function RadioInput({
 }) {
   return (
     <div
-      className={`border-alt-50 hover:border-alt-200 ${selected ? "border-primary! py-3" : ""} flex cursor-pointer items-center gap-3 rounded border px-3 py-1 transition-all not-last:mb-1`}
+      className={`border-alt-50 hover:border-alt-200 ${selected ? "border-primary! bg-primary-100 py-3" : ""} flex cursor-pointer items-center gap-3 rounded border px-3 py-1 transition-all not-last:mb-1`}
       role="radio"
       onClick={() => setSelected(value)}
     >
       <span
-        className={`border-alt-400 flex h-4 w-4 items-center justify-center rounded-full border transition-all ${selected ? "bg-primary border-primary-300 h-6 w-6" : ""}`}
+        className={`border-alt-400 flex h-4 w-4 items-center justify-center rounded-full border transition-all ${selected ? "bg-primary-400 border-primary h-6 w-6" : ""}`}
       >
         {selected && (
           <Check className="text-primary-off-800" size={16} strokeWidth={3} />
